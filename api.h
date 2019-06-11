d35c2fc648e5da6d2a38e23832b5641a007bb4bc
LeonDeHill <leon.hillmann@googlemail.com>
2019-06-11 22:07:47 +0200

    Switched language

--- api.h
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-
-using namespace std;
-
-void helloWorld()
-{
-    cout << "Hello World!" << endl;
-}
--- main.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-#include "api.h"
-using namespace std;
-int main()
-{
-    helloWorld();
-}
